2cc6080684eb8a3f154224e3aecddbf43a3d4dcf
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2024-03-02 14:47:50 +0700

    WD: Feeds

--- InstagramCloneV2/app/src/main/java/com/example/instagramclonev2/adapters/StoryAdapter.kt
+++ InstagramCloneV2/app/src/main/java/com/example/instagramclonev2/adapters/StoryAdapter.kt
@@ -1,20 +1,24 @@
 package com.example.instagramclonev2.adapters
+
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagramclonev2.databinding.StoryItemBinding
 import com.example.instagramclonev2.models.InstaStories
 import coil.load
+import com.example.instagramclonev2.FullSizeStoryActivity
 
-class StoryAdapter(private val instaStories: List<InstaStories>)
-    : RecyclerView.Adapter<StoryAdapter.RecyclerViewHolder>() {
-    class RecyclerViewHolder ( val binding: StoryItemBinding) : RecyclerView.ViewHolder(binding.root)
+class StoryAdapter(
+    private val context: Context,
+    private val instaStories: List<InstaStories>
+) : RecyclerView.Adapter<StoryAdapter.RecyclerViewHolder>() {
+
+    class RecyclerViewHolder(val binding: StoryItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
         val binding = StoryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-
         return RecyclerViewHolder(binding)
     }
 
@@ -22,8 +26,14 @@ class StoryAdapter(private val instaStories: List<InstaStories>)
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
         val story = instaStories[position]
-        holder.binding.tvStoryUserName.text = story.userName.toString()
-        holder.binding.ivStoryImg.load(story.storyImg)
-
+        holder.binding.tvStoryUserName.text = story.user?.displayName.toString()
+        holder.binding.ivStoryImg.load(story.user?.profileImg)
+        holder.binding.ivStoryImg.setOnClickListener {
+            val intent = Intent(context, FullSizeStoryActivity::class.java)
+            intent.putExtra("image_url", story.storyImageUrl)
+            intent.putExtra("user_name", story.user?.displayName)
+            intent.putExtra("profile_img", story.user?.profileImg)
+            context.startActivity(intent)
+        }
     }
 }
